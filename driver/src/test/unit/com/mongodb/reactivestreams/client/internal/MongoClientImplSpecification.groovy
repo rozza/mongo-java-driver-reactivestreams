0f2336f5f529c7ee1d11a73812f8fe7fb8986b68
Ross Lawley <ross.lawley@gmail.com>
2017-12-07 14:33:53 +0000

    MongoDB 3.6 Support
    Updated the Mongo Async Java Driver to 3.6.0 and updated
    the API to match the new API.  This includes support for
    Client Sessions, updates to List Databases and Aggregation.
    
    JAVARS-41

--- driver/src/test/unit/com/mongodb/reactivestreams/client/internal/MongoClientImplSpecification.groovy
+++ driver/src/test/unit/com/mongodb/reactivestreams/client/internal/MongoClientImplSpecification.groovy
@@ -19,6 +19,7 @@ package com.mongodb.reactivestreams.client.internal
 import com.mongodb.async.client.ListDatabasesIterable
 import com.mongodb.async.client.MongoClient as WrappedMongoClient
 import com.mongodb.reactivestreams.client.MongoClient
+import com.mongodb.session.ClientSession
 import org.bson.BsonDocument
 import org.bson.Document
 import spock.lang.Specification
@@ -50,35 +51,55 @@ class MongoClientImplSpecification extends Specification {
 
     def 'should call the underlying listDatabases'() {
         given:
+        def clientSession = Stub(ClientSession)
         def wrappedResult = Stub(ListDatabasesIterable)
-        def wrapped = Mock(WrappedMongoClient) {
-            1 * listDatabases(Document) >> wrappedResult
-            1 * listDatabases(BsonDocument) >> wrappedResult
-        }
+        def wrapped = Mock(WrappedMongoClient)
         def mongoClient = new MongoClientImpl(wrapped)
-
 
         when:
         def publisher = mongoClient.listDatabases()
 
         then:
+        1 * wrapped.listDatabases(Document) >> wrappedResult
         expect publisher, isTheSameAs(new ListDatabasesPublisherImpl(wrappedResult))
 
         when:
         publisher = mongoClient.listDatabases(BsonDocument)
 
         then:
+        1 * wrapped.listDatabases(BsonDocument) >> wrappedResult
         expect publisher, isTheSameAs(new ListDatabasesPublisherImpl(wrappedResult))
 
+        when:
+        publisher = mongoClient.listDatabases(clientSession)
 
+        then:
+        1 * wrapped.listDatabases(clientSession, Document) >> wrappedResult
+        expect publisher, isTheSameAs(new ListDatabasesPublisherImpl(wrappedResult))
+
+        when:
+        publisher = mongoClient.listDatabases(clientSession, BsonDocument)
+
+        then:
+        1 * wrapped.listDatabases(clientSession, BsonDocument) >> wrappedResult
+        expect publisher, isTheSameAs(new ListDatabasesPublisherImpl(wrappedResult))
     }
 
     def 'should call the underlying listDatabaseNames'() {
+        given:
+        def clientSession = Stub(ClientSession)
+
         when:
         mongoClient.listDatabaseNames()
 
         then:
         1 * wrapped.listDatabaseNames()
+
+        when:
+        mongoClient.listDatabaseNames(clientSession)
+
+        then:
+        1 * wrapped.listDatabaseNames(clientSession)
     }
 
 }
